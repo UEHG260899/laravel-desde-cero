4fc8cf5d288e131cad56411dcd5394956bbab049
Uriel <urielenrique99@gmail.com>
2021-12-27 21:17:06 -0600

    added laravel template

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Lista de productos</h1>
+@extends('layouts.master')
 
+@section('content')
+    <h1>Lista de productos</h1>
+    
     @empty($products)
         <div class="alert alert-warning">
             The list of products is empty
@@ -41,6 +35,4 @@
             </table>
         </div>
     @endempty
-    
-</body>
-</html>
+@endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
     <h1>{{ $product->title }} ({{ $product->id }})</h1>
     <p>{{ $product->description }}</p>
     <p>{{ $product->price }}</p>
     <p>{{ $product->stock }}</p>
     <p>{{ $product->status }}</p>
-
+    
     <!--{!! $html !!}-->
-</body>
-</html>
+@endsection
+    
