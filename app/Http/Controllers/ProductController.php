10552dec5d7e28990df4bf76fff04d6a9c3c4a4e
Uriel Enrique <urielenrique99@gmail.com>
2022-01-25 21:06:01 -0600

    created products component

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,13 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
+    
+    public function __construct()
+    {   
+        $this->middleware('auth');
+    }
+    
+    
     public function index()
     {
         $products = Product::all();
